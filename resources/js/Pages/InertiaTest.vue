58101e320e8a7f87842d0a6f2885b5b0aae7ce94
Harada Nobuhiro <nobu@HaradanoMacBook-Air.local>
2023-05-05 20:42:27 +0900

    Inertiaルーティングテスト

---
+++ resources/js/Pages/InertiaTest.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { Link } from '@inertiajs/inertia-vue3';
+</script>
+
+<template>
+    Inertiaテストです。
+    <br>
+    <a href="/">aタグ経由です。</a>
+    <br>
+    <Link href="/">Linkタグ経由です。</Link>
+    <br>
+    <Link :href="route('inertia.index')">名前付きルートの確認です。</Link>
+    <br>
+    <Link :href="route('inertia.show', { id : 1 })">ルートパラメータの確認です。</Link>
+</template>
