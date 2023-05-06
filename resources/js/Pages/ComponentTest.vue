65d2455f13f7ab9949a1bedf6352046cf806dca0
Harada Nobuhiro <nobu@HaradanoMacBook-Air.local>
2023-05-06 22:06:07 +0900

    コンポーネントテスト

---
+++ resources/js/Pages/ComponentTest.vue
@@ -0,0 +1,14 @@
+<script setup>
+import GuestLayout from '@/Layouts/GuestLayout.vue';
+import Label from '@/Components/InputLabel.vue'
+import Input from '@/Components/TextInput.vue'
+</script>
+
+<template>
+<GuestLayout>
+    <Label value="件名">タイトル1</Label>
+    <Label value="">タイトル2</Label>
+    <Label>タイトル3</Label>
+    <Input modelValue="初期値が入ります"></Input>
+</GuestLayout>
+</template>
