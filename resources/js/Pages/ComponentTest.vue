2ee7f1700360a9e16a1b46131d328cc8376f7a3e
Harada Nobuhiro <nobu@HaradanoMacBook-Air.local>
2023-05-06 22:13:37 +0900

    defineEmitテスト

--- resources/js/Pages/ComponentTest.vue
+++ resources/js/Pages/ComponentTest.vue
@@ -2,6 +2,8 @@
 import GuestLayout from '@/Layouts/GuestLayout.vue';
 import Label from '@/Components/InputLabel.vue'
 import Input from '@/Components/TextInput.vue'
+
+const emitTest = e => console.log(e)
 </script>
 
 <template>
@@ -9,6 +11,10 @@ import Input from '@/Components/TextInput.vue'
     <Label value="件名">タイトル1</Label>
     <Label value="">タイトル2</Label>
     <Label>タイトル3</Label>
-    <Input modelValue="初期値が入ります"></Input>
+    <Input
+        modelValue="初期値が入ります"
+        @update:modelValue="emitTest"
+    >
+    </Input>
 </GuestLayout>
 </template>
