fd63218e27ad1bd17d39c3f57f258c6eab4e3aff
Harada Nobuhiro <nobu@HaradanoMacBook-Air.local>
2023-05-06 19:35:24 +0900

    フラッシュメッセージテスト

--- resources/js/Pages/Inertia/Index.vue
+++ resources/js/Pages/Inertia/Index.vue
@@ -3,5 +3,8 @@ import { Link } from '@inertiajs/inertia-vue3';
 </script>
 
 <template>
+    <div v-if="$page.props.flash.message" class="bg-blue-300">
+        {{ $page.props.flash.message }}
+    </div>
 ああああ
 </template>
