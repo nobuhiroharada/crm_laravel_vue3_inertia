0c88d403aec29ca6c40f76bdea93276adcd02cee
Harada Nobuhiro <nobu@HaradanoMacBook-Air.local>
2023-05-12 22:13:58 +0900

    商品購入中間テーブル作成

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Customer;
+use App\Models\Purchase;
+use App\Models\Item;
 
 class DatabaseSeeder extends Seeder
 {
@@ -19,8 +22,17 @@ class DatabaseSeeder extends Seeder
             ItemSeeder::class,
         ]);
 
-        \App\Models\Customer::factory(1000)->create();
-        \App\Models\Purchase::factory(100)->create();
+        Customer::factory(1000)->create();
+
+        $items = Item::all();
+        Purchase::factory(100)->create()
+            ->each(function(Purchase $purchase) use ($items) {
+                $purchase->items()->attach(
+                    // 1~3個のitemをpurchaseにランダムに紐付け
+                    $items->random(rand(1, 3))->pluck('id')->toArray(),
+                    ['quantity' => rand(1, 5)]
+                );
+            });
 
         // \App\Models\User::factory(10)->create();
 
