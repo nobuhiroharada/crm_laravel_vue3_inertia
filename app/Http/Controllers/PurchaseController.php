2f197ff0009acc6b721e3d452d503e88238ecc9e
Harada Nobuhiro <nobu@HaradanoMacBook-Air.local>
2023-05-14 21:18:50 +0900

    商品購入画面、処理追加

--- app/Http/Controllers/PurchaseController.php
+++ app/Http/Controllers/PurchaseController.php
@@ -4,7 +4,11 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\StorePurchaseRequest;
 use App\Http\Requests\UpdatePurchaseRequest;
+use Illuminate\Support\Facades\DB;
+use Inertia\Inertia;
 use App\Models\Purchase;
+use App\Models\Customer;
+use App\Models\Item;
 
 class PurchaseController extends Controller
 {
@@ -25,7 +29,11 @@ class PurchaseController extends Controller
      */
     public function create()
     {
-        //
+        $items = Item::select('id', 'name', 'price')->where('is_selling', true)->get();
+
+        return Inertia::render('Purchases/Create', [
+            'items' => $items,
+        ]);
     }
 
     /**
@@ -36,7 +44,27 @@ class PurchaseController extends Controller
      */
     public function store(StorePurchaseRequest $request)
     {
-        //
+        DB::beginTransaction();
+
+        try {
+            $purchase = Purchase::create([
+                'customer_id' => $request->customer_id,
+                'status' => $request->status,
+            ]);
+    
+            foreach($request->items as $item) {
+                $purchase->items()->attach($purchase->id, [
+                    'item_id' => $item['id'],
+                    'quantity' => $item['quantity'],
+                ]);
+            }
+
+            DB::commit();
+    
+            return to_route('dashboard');
+        } catch(\Exception $e) {
+            DB::rollBack();
+        }
     }
 
     /**
